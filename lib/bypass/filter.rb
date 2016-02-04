40714a3e18f083801b0eb78059c2a4b6ae3241fc
amandawraymond <amandawraymond@gmail.com>
2016-02-04 11:01:23 -0500

    working auto link feature for finding urls with or without a protocol but not email addresses

--- lib/bypass/filter.rb
+++ lib/bypass/filter.rb
@@ -1,34 +1,39 @@
-# module Bypass
-#   class Filter
-#     attr_reader :content, :fragment
+module Bypass
+  class Filter
+    attr_reader :content, :fragment
     
-#     URL_PATTERN = /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[\-;:&=\+\$,\w]+@)?[A-Za-z0-9\.\-]+|(?:www\.|[\-;:&=\+\$,\w]+@)[A-Za-z0-9\.\-]+)((?:\/[\+~%\/\.\w\-]*)?\??(?:[\-\+=&;%@\.\w]*)#?(?:[\.\!\/\\\w]*))?)/g;
-    
-#     def initialize(content, options = {})
-#       @content = content.to_s.encode("UTF-8")
-#       @fragment = options.fetch(:fragment, true)
-#     end
+    URL_PATTERN = %r{
+              (?: ((?:ed2k|ftp|http|https|irc|mailto|news|gopher|nntp|telnet|webcal|xmpp|callto|feed|svn|urn|aim|rsync|tag|ssh|sftp|rtsp|afs|file):)// | www\. )
+              [^\s<\u00A0"]+
+            }ix
 
-#     def replace
-#       raise NotImplementedError
-#     end
-    
-#     def to_s
-#       content
-#     end
-  
-#   private
-  
-#     def gsub_urls(text, &block)
-#       text.gsub(URL_PATTERN) do |match|
-#         yield(match.to_s)
-#       end
-#     end
+	def initialize(content, options = {})
+		@content = content.to_s.encode("UTF-8")
+		@fragment = options.fetch(:fragment, true)
+	end
 
-#     def parse_uri(uri)
-#       Bypass::URI.parse(uri.to_s.strip)
-#     rescue => ex
-#       nil
-#     end
-#   end
-# end
+	def replace
+		raise NotImplementedError
+	end
+
+	def to_s
+		content
+	end
+  
+	private
+  
+	    def gsub_urls(text, &block)
+			text.gsub(URL_PATTERN) do |url|	
+				url = "http://" + url unless (url.match(/^http:\/\//i) || url.match(/^https:\/\//i))
+				yield(url.to_s)
+			end
+	    end
+
+	    def parse_uri(uri)
+	      Bypass::URI.parse(uri.to_s.strip)
+	    rescue => ex
+	      nil
+	    end
+
+  	end
+end
