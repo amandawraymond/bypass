1e452e5a57585db90900a4ee13f0c6953c6d9d9f
amandawraymond <amandawraymond@gmail.com>
2016-02-04 01:02:54 -0500

    comment out test

--- lib/bypass/filter.rb
+++ lib/bypass/filter.rb
@@ -1,34 +1,34 @@
-module Bypass
-  class Filter
-    attr_reader :content, :fragment
+# module Bypass
+#   class Filter
+#     attr_reader :content, :fragment
     
-    URL_PATTERN = /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[\-;:&=\+\$,\w]+@)?[A-Za-z0-9\.\-]+|(?:www\.|[\-;:&=\+\$,\w]+@)[A-Za-z0-9\.\-]+)((?:\/[\+~%\/\.\w\-]*)?\??(?:[\-\+=&;%@\.\w]*)#?(?:[\.\!\/\\\w]*))?)/g;
+#     URL_PATTERN = /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[\-;:&=\+\$,\w]+@)?[A-Za-z0-9\.\-]+|(?:www\.|[\-;:&=\+\$,\w]+@)[A-Za-z0-9\.\-]+)((?:\/[\+~%\/\.\w\-]*)?\??(?:[\-\+=&;%@\.\w]*)#?(?:[\.\!\/\\\w]*))?)/g;
     
-    def initialize(content, options = {})
-      @content = content.to_s.encode("UTF-8")
-      @fragment = options.fetch(:fragment, true)
-    end
+#     def initialize(content, options = {})
+#       @content = content.to_s.encode("UTF-8")
+#       @fragment = options.fetch(:fragment, true)
+#     end
 
-    def replace
-      raise NotImplementedError
-    end
+#     def replace
+#       raise NotImplementedError
+#     end
     
-    def to_s
-      content
-    end
+#     def to_s
+#       content
+#     end
   
-  private
+#   private
   
-    def gsub_urls(text, &block)
-      text.gsub(URL_PATTERN) do |match|
-        yield(match.to_s)
-      end
-    end
+#     def gsub_urls(text, &block)
+#       text.gsub(URL_PATTERN) do |match|
+#         yield(match.to_s)
+#       end
+#     end
 
-    def parse_uri(uri)
-      Bypass::URI.parse(uri.to_s.strip)
-    rescue => ex
-      nil
-    end
-  end
-end
+#     def parse_uri(uri)
+#       Bypass::URI.parse(uri.to_s.strip)
+#     rescue => ex
+#       nil
+#     end
+#   end
+# end
