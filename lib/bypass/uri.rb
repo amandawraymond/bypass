adcec439bb6b6b92d0937c649321daa195f42b5b
amandawraymond <amandawraymond@gmail.com>
2016-02-04 00:50:32 -0500

    commented out

--- lib/bypass/uri.rb
+++ lib/bypass/uri.rb
@@ -1,19 +1,19 @@
-require "addressable/uri"
+# require "addressable/uri"
 
-module Bypass
-  class URI < Addressable::URI
-    binding.remote_pry
-    URIREGEX = /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[\-;:&=\+\$,\w]+@)?[A-Za-z0-9\.\-]+|(?:www\.|[\-;:&=\+\$,\w]+@)[A-Za-z0-9\.\-]+)((?:\/[\+~%\/\.\w\-]*)?\??(?:[\-\+=&;%@\.\w]*)#?(?:[\.\!\/\\\w]*))?)/g;
+# module Bypass
+#   class URI < Addressable::URI
+    
+#     URIREGEX = /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[\-;:&=\+\$,\w]+@)?[A-Za-z0-9\.\-]+|(?:www\.|[\-;:&=\+\$,\w]+@)[A-Za-z0-9\.\-]+)((?:\/[\+~%\/\.\w\-]*)?\??(?:[\-\+=&;%@\.\w]*)#?(?:[\.\!\/\\\w]*))?)/g;
 
-    def append_to_query_values(params = {})
-      pairs = params.keys.map do |raw_key|
-        key = CGI.escape(raw_key.to_s)
-        val = CGI.escape(params[raw_key])
-        [key, val].join("=")
-      end
+#     def append_to_query_values(params = {})
+#       pairs = params.keys.map do |raw_key|
+#         key = CGI.escape(raw_key.to_s)
+#         val = CGI.escape(params[raw_key])
+#         [key, val].join("=")
+#       end
 
-      pairs.unshift(query) if query
-      self.query = pairs.join("&")
-    end
-  end
-end
+#       pairs.unshift(query) if query
+#       self.query = pairs.join("&")
+#     end
+#   end
+# end
